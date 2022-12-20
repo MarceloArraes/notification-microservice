a9670611ff334c7369aac3aa79c16f6c7347e20a
MarceloArraes <marcelo.arraes@gmail.com>
2022-12-20 11:55:15 +0000

    feat: creating a cancelation for notification

---
+++ src/application/use-cases/cancel-notification.ts
@@ -0,0 +1,29 @@
+import { Content } from '../entities/notification-content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface CancelNotificationRequest {
+  notificationId: string;
+}
+
+/* interface CancelNotificationResponse {
+  notification: Notification;
+} */
+type CancelNotificationResponse = void;
+
+@Injectable()
+export class CancelNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+  async execute(
+    request: CancelNotificationRequest,
+  ): Promise<CancelNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationsRepository.findById(
+      notificationId,
+    );
+    if (!notification) throw new NotificationNotFound();
+  }
+}
